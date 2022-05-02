6ff7737e93bc600476e34e246f990f431be06750
No.7 <33@33.com>
2022-05-02 22:59:51 +0800

    remove-duplicates-from-sorted-array  删除有序数组中的重复项

---
+++ Poplar.Algorithm.Array/Easy/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poplar.Algorithm.Array
+{
+    internal class RemoveDuplicatesFromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            var slow = 1;
+            for (var i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1]) nums[slow++] = nums[i];
+            }
+            return slow;
+        }
+    }
+}
--- Poplar.Algorithm.Array/Program.cs
+++ Poplar.Algorithm.Array/Program.cs
@@ -7,6 +7,8 @@ using Poplar.Algorithm.Array.Medium;
 
 //new MergeSortedArray().MergeTwo(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3);
 
-new RotateArray().RotateThree(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3);
+//new RotateArray().RotateThree(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3);
+
+new RemoveDuplicatesFromSortedArray().RemoveDuplicates(new int[] { 1, 1, 2 });
 
 Console.WriteLine("Hello, World!");
