b63859e5b40e05c3877c498ddb90a47486a67ada
No.7 <33@33.com>
2022-05-02 23:02:23 +0800

    remove-duplicates-from-sorted-array  删除有序数组中的重复项    增加备注

--- Poplar.Algorithm.Array/Easy/RemoveDuplicatesFromSortedArray.cs
+++ Poplar.Algorithm.Array/Easy/RemoveDuplicatesFromSortedArray.cs
@@ -8,12 +8,18 @@ namespace Poplar.Algorithm.Array
 {
     internal class RemoveDuplicatesFromSortedArray
     {
+        /// <summary>
+        /// 有一个快指针fast和一个慢指针slow。
+        /// 对快指针fast，当nums[fast] != nums[fast - 1]时，nums[fast]值和上一个值是不重复的，将num[slow]指向nums[fast]，slow++。
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
         public int RemoveDuplicates(int[] nums)
         {
             var slow = 1;
-            for (var i = 1; i < nums.Length; i++)
+            for (var fast = 1; fast < nums.Length; fast++)
             {
-                if (nums[i] != nums[i - 1]) nums[slow++] = nums[i];
+                if (nums[fast] != nums[fast - 1]) nums[slow++] = nums[fast];
             }
             return slow;
         }
